3d607938ae31d32604d075bf66cc956c72208d36
VBeliaev <vbeliaev@luxoft.com>
2017-12-10 23:44:17 +0200

    Fix issue #5

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -2,14 +2,19 @@ import pytest
 from my_app.fibonacci import fib
 
 
+@pytest.mark.smoke
+@pytest.mark.positive
 def test_smoke():
     assert fib(1)
 
 
+@pytest.mark.smoke
+@pytest.mark.positive
 def test_type():
     assert type(fib(1)) == list
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("test_input,expected", [
     (1, [0]),
     (2, [0, 1]),
@@ -20,8 +25,9 @@ def test_positive(test_input, expected):
     assert fib(test_input) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("test_input,expected,message", [
-    (0,  ValueError, "Number must be a positive"),
+    (0, ValueError, "Number must be a positive"),
     (-3, ValueError, "Number must be a positive"),
     (1.0, TypeError, ".*"),
     ((1, 1), TypeError, ".*"),
@@ -29,8 +35,3 @@ def test_positive(test_input, expected):
 def test_neg_str(test_input, expected, message):
     with pytest.raises(expected, match=message):
         fib(test_input)
-
-
-@pytest.mark.skip
-def test_some_todo_test():
-    pass
--- tests/test_pairs.py
+++ tests/test_pairs.py
@@ -2,14 +2,19 @@ import pytest
 from my_app.numbers_pairs import pairs
 
 
+@pytest.mark.smoke
+@pytest.mark.positive
 def test_smoke():
     assert pairs([1, 9])
 
 
+@pytest.mark.smoke
+@pytest.mark.positive
 def test_type():
     assert type(pairs([1, 9])) == list
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("test_input,expected", [
     ([1, 9, 5, 5, 3, 8], (1, 9)),
     ([1, 9, 5, 5, 3, 8], (5, 5)),
@@ -18,29 +23,23 @@ def test_positive(test_input, expected):
     assert expected in pairs(test_input)
 
 
+@pytest.mark.negative
 def test_results_uniqueness():
     assert (9, 1) not in pairs([1, 9, 5, 5, 3, 1])
 
 
+@pytest.mark.negative
 def test_results_uniqueness_2():
     assert pairs([5, 5, 5, 5]).count((5, 5)) == 1
 
 
-# @pytest.mark.debug
-def test_sum():
-    assert pairs([1, 9, 4], s=5) == [(1, 4)]
-
-
-def test_all_pairs():
-    assert pairs([1, 9, 1], all_pairs=True) == [(1, 9), (9, 1)]
-
-
+@pytest.mark.negative
 @pytest.mark.parametrize("test_input,expected,exp_message", [
     ([1], ValueError, "At least 2 numbers required"),
     (1, TypeError, "Only list of digits accepted"),
     ([1, "a"], TypeError, "Only list of digits accepted"),
 ])
-def test_few_numbers(test_input, expected, exp_message):
+def test_negative(test_input, expected, exp_message):
     with pytest.raises(expected, match=exp_message):
         pairs(test_input)
 
