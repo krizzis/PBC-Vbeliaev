d1e5585342b78c1dc51981f2639f4d7a5a926c66
krizzis <dn301182bva@gmail.com>
2017-12-14 13:13:59 +0200

    Add check http request test

--- pbc/sel_grid/test_sel_grid.py
+++ pbc/sel_grid/test_sel_grid.py
@@ -1,12 +1,13 @@
-from time import sleep
-
 import pytest
+import requests
+
+from pbc.sel_grid import HOST_NAME
 from pbc.sel_grid.sel_grid import Grid, StartGrid
 from pbc.sel_grid.page import GridPage
 
 
 @pytest.mark.smoke
-# @pytest.mark.selenium
+@pytest.mark.selenium
 def test_grid_install_smoke(ssh_client):
     grid = StartGrid(Grid(ssh_client))
     grid.download()
@@ -24,3 +25,13 @@ def test_grid_numbers_of_session(ssh_client, firefox_driver):
     grid.add_node()
     page = GridPage(firefox_driver)
     assert page.max_sessions() == 5
+
+
+@pytest.mark.http
+def test_grid_session_count_http(ssh_client):
+    grid = StartGrid(Grid(ssh_client))
+    grid.download()
+    grid.start_hub()
+    grid.add_node()
+    response = requests.get('http://{}:4444/grid/console'.format(HOST_NAME))
+    assert response.content.count('browserName=firefox') == 5
